bb9effdd6d8824d63b219c9cc6bbffb8e68f06cf
SunX-1224 <sunilsapkota1224@gmail.com>
2023-11-09 00:01:32 +0545

    feat: camera tilt and dynamic fov

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float initialFOV = 60f;
+    public float zoomFOV = 75f;
+    
+    private Coroutine fovCoroutine; 
+
+    Camera cam;
+    void Start(){
+        cam = GetComponent<Camera>();
+        cam.fieldOfView = initialFOV;
+    }
+
+    public void IncreaseFOV(float speed){
+        if(fovCoroutine != null){
+            StopCoroutine(fovCoroutine);
+        }
+        fovCoroutine = StartCoroutine(ChangeFOV(zoomFOV, speed));
+    }
+
+    public void ResetFOV(float speed){
+        if(fovCoroutine != null){
+            StopCoroutine(fovCoroutine);
+        }
+        fovCoroutine = StartCoroutine(ChangeFOV(initialFOV, speed));
+    }
+
+    IEnumerator ChangeFOV(float targetFOV, float speed){
+        float currentFOV = cam.fieldOfView;
+
+        while(Mathf.Abs(currentFOV - targetFOV) > 0.01f){
+            currentFOV = Mathf.Lerp(currentFOV, targetFOV, Time.deltaTime * speed);
+            cam.fieldOfView = currentFOV;
+            yield return null;
+        }
+
+        cam.fieldOfView = targetFOV;
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,47 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody rb;
-    Camera cam;
+    private Rigidbody rb;
+    private CameraController camController;
 
-    float initialFOV;
-    float pushedFOV;
+    public float thrust;
+    public float boost;
+    public float turnPower;
+    public float jumpPower;
 
-    public float thrust = 3.0f;
-    public float boost = 1.0f;
-    public float turnPower = 5.0f;
+    public Vector3 maxTilt = new Vector3(20, 0, 30);
+    private Coroutine tiltCoroutine;
+    private Vector3 targetTilt = new Vector3(0,0,0);
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        cam = transform.Find("Camera").GetComponent<Camera>();
-        initialFOV = cam.fieldOfView;
-        pushedFOV = initialFOV + 10f;
+        camController = GetComponentInChildren<Camera>().GetComponent<CameraController>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 force = thrust * Vector3.forward;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            force += Vector3.forward * boost;
-            cam.fieldOfView = pushedFOV;
-        }
-        else
-        {
-            cam.fieldOfView = initialFOV;
+
+        Vector3 force = new(0,0,0);
+        force.z = thrust;
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+            force.z += boost;
+            camController.IncreaseFOV(boost + thrust);
+        }else{
+            camController.ResetFOV(3.0f);
+            if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
+                force.z -= boost;
+                targetTilt.x = -maxTilt.x;
+            }else{
+                targetTilt.x = 0f;
+            }
         }
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-            force += Vector3.left * turnPower;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-            force += Vector3.right * turnPower;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            force.x -= turnPower;
+            targetTilt.z = maxTilt.z;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            force.x += turnPower;
+            targetTilt.z = -maxTilt.z;
+        }
+        else{
+            targetTilt.z = 0f;
+        }
+
+        if(rb.useGravity && transform.position.y < 0f){
+            rb.useGravity = false;
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY;
+        }
+
+        if(Input.GetKey(KeyCode.Space)){
+            if(!rb.useGravity) {
+                rb.constraints = RigidbodyConstraints.FreezeRotationY;
+                rb.AddForce(0,jumpPower, 0);
+                rb.useGravity = true;
+            }
+        }
 
         rb.AddForce(force);
     }
 
+    void Update()
+    {
+        TiltPlayer(targetTilt, turnPower);
+    }
 
+    void TiltPlayer(Vector3 targetTilt, float speed)
+    {
+        Quaternion targetRotation = Quaternion.Euler(targetTilt);
+        transform.localRotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * speed);
+    }
 }
+
